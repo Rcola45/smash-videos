fada10d2548a1ea9b15d14a2d51d1104c1901a25
Ryan Colaiacovo <rcola45@hotmail.com>
2019-08-07 21:37:19 -0300

    login for sidekiq prod, ignore redis dump file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   require 'sidekiq/web'
   require 'sidekiq/cron/web'
-
-  mount Sidekiq::Web, at: "/sidekiq"
   
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    # Protect against timing attacks: (https://codahale.com/a-lesson-in-timing-attacks/)
+    # - Use & (do not use &&) so that it doesn't short circuit.
+    # - Use `secure_compare` to stop length information leaking
+    ActiveSupport::SecurityUtils.secure_compare(username, ENV["SIDEKIQ_USERNAME"]) &
+      ActiveSupport::SecurityUtils.secure_compare(password, ENV["SIDEKIQ_PASSWORD"])
+  end if Rails.env.production?
+  mount Sidekiq::Web, at: "/sidekiq"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   match 'youtube_callback', to: 'videos#youtube_callback', via: [:post, :get], defaults: {format: :json}
   root to: 'videos#index'
