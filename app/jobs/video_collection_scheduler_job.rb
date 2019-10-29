05b3d361fb3ede2bb05672e8e23bcad7e3499b70
Ryan Colaiacovo <rcola45@hotmail.com>
2019-10-29 19:19:30 -0300

    commenting out scheduler job for now. Will run manually

--- app/jobs/video_collection_scheduler_job.rb
+++ app/jobs/video_collection_scheduler_job.rb
@@ -1,6 +1,6 @@
 class VideoCollectionSchedulerJob < ApplicationJob
   Source.active.find_each do |source|
-    delay = rand(1..60).minutes
-    CollectVideosJob.set(wait: delay).perform_later()
+    # delay = rand(1..60).minutes
+    # CollectVideosJob.set(wait: delay).perform_later()
   end
 end
