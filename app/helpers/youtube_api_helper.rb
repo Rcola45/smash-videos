3661d829275482cedead97a4b9bbb1ec99e661fa
Ryan Colaiacovo <rcola45@hotmail.com>
2020-02-03 19:28:19 -0400

    moved youtube application name to env

--- app/helpers/youtube_api_helper.rb
+++ app/helpers/youtube_api_helper.rb
@@ -4,7 +4,7 @@ module YoutubeApiHelper
   def youtube_service
     service = Google::Apis::YoutubeV3::YouTubeService.new
     service.key = ENV['YOUTUBE_DATA_API_KEY']
-    service.client_options.application_name = 'Smash Collection'
+    service.client_options.application_name = ENV['YOUTUBE_APPLICATION_NAME']
     service.authorization = nil
 
     service
